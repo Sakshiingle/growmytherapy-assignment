71eaca74f7e3fe6867535512a078f8e403a660e9
Sakshi Ingle <isakshi713@gmail.com>
2026-02-13 00:35:21 +0530

    Fix image basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
+    unoptimized: true,
   },
+  basePath: '/growmytherapy-assignment',
+  assetPrefix: '/growmytherapy-assignment/',
 };
 
 module.exports = nextConfig;
