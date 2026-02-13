3baf7efd386b873cd03883ca25fcbeadceaf6fdd
Sakshi Ingle <isakshi713@gmail.com>
2026-02-13 12:24:00 +0530

    Solving Image Error

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '/growmytherapy-assignment',
-  assetPrefix: '/growmytherapy-assignment/',
+  basePath: isProd ? '/growmytherapy-assignment' : '',
+  assetPrefix: isProd ? '/growmytherapy-assignment/' : '',
 };
+
 
 module.exports = nextConfig;
